182f8f9fb2fee848c1b7040b503179189b387c81
TheMultivariateAnalyst <aravindkarthik200@gmail.com>
2023-05-15 13:09:45 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,27 @@
+import sys
+import logging
+
+logging.basicConfig(filename='05_15_2023_12_46_20.log', level=logging.INFO)
+
+def error_message_detail(error, error_detail):
+    _, _, exc_tb = error_detail
+    file_name = exc_tb.tb_frame.f_code.co_filename
+    line_number = exc_tb.tb_lineno
+    error_message = "Error occurred in Python script: {0}, Line number: {1}, Error message: {2}".format(file_name, line_number, str(error))
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error, error_detail):
+        super().__init__(error)
+        self.error_message = error_message_detail(error, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+if __name__ == '__main__':
+    try:
+        a = 1 / 0
+    except Exception as e:
+        logging.info("Dividing by zero error")
+        raise CustomException(e, sys.exc_info())
